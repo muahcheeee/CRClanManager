fe0ab9271500cec8d38aad1e797503020e27ea0f
CheeYeo <tehcy2@msn.com>
2017-09-29 01:21:36 +0800

    Print weekly clan report

--- CRClanManager.py
+++ CRClanManager.py
@@ -15,8 +15,32 @@ rawData = json.loads(clanJsonData.text)
 numMembers = len(rawData['members'])
 
 clanData = []
-print(rawData['members'][0]['name'])
 for i in range(numMembers):
-    clanData.append([rawData['members'][i]['name'],int(rawData['members'][i]['donations']),int(rawData['members'][i]['clanChestCrowns'])])
+    clanData.append([rawData['members'][i]['name'],rawData['members'][i]['donations'],rawData['members'][i]['clanChestCrowns']])
 
-print(clanData)
+#Sort according to donations
+clanData.sort(key=lambda x: x[1])
+print("Weekly Clan Report")
+print("")
+print("Top 3 Donators")
+for i in range(3):
+    print("No.",str(i+1), ": ", clanData[numMembers-(i+ 1)][0] , "(", clanData[numMembers-(i +1)][1], ")")
+
+print("")
+print("Members with less than 100 Donations")
+for i in range(numMembers):
+    if (int(clanData[i][1] < 100)):
+        print(clanData[i][0], "(", clanData[i][1], ")")
+
+clanData.sort(key=lambda x: x[2])
+
+print("")
+print("Top 3 Clan Chest Contributers")
+for i in range(3):
+    print("No.",str(i+1), ": ", clanData[numMembers-(i+ 1)][0] , "(", clanData[numMembers-(i +1)][2], ")")
+
+print("")
+print("Members with less than 10 Clan Chest Crowns")
+for i in range(numMembers):
+    if (int(clanData[i][2] < 10)):
+        print(clanData[i][0], "(", clanData[i][2], ")")
